73cf29105e24c78f6a8ca52923e6f7bf3ed31bff
Hyolo <soejdtodxn@gmail.com>
2023-04-08 01:06:16 +0900

    update_row 함수정리

--- venv/app.py
+++ venv/app.py
@@ -1,5 +1,5 @@
 from flask import Flask, request
-from notion_manager import find_row_in_database, update_row
+from notion_manager import find_row_in_database, increase_and_update_row
 from notion_client import Client
 from pprint import pprint
 
@@ -7,17 +7,12 @@ app = Flask(__name__)
 
 NOTION_API_KEY = "secret_mqg8dyuo729AtFeHReI4U0sMfCNslzvJVrAiCysNsxL"
 DB_ID = "9c0e71c0f9c645089fdbba0126762d80"
+ROW_NAME = "CodeWars"
 
-@app.route("/", methods=['GET', 'POST'])
+@app.route("/commit-count/", methods=['GET', 'POST'])
 def notion_api_test():
-    rows = find_row_in_database("CodeWars")
-    if rows:
-        row = rows[0]
-        commit_count = row['properties']['CommitCount']['number']
-        update_row(row['id'], "CommitCount", commit_count + 1)
-    else:
-        pprint(f"No rows")
-
+    rows = find_row_in_database(ROW_NAME)
+    increase_and_update_row(rows)
     return ""
 
 if __name__ == '__main__':
--- venv/notion_manager.py
+++ venv/notion_manager.py
@@ -1,4 +1,5 @@
 from notion_client import Client
+from pprint import pprint
 
 NOTION_API_KEY = "secret_mqg8dyuo729AtFeHReI4U0sMfCNslzvJVrAiCysNsxL"
 DB_ID = "9c0e71c0f9c645089fdbba0126762d80"
@@ -7,6 +8,14 @@ DB_ID = "9c0e71c0f9c645089fdbba0126762d80"
 def find_row_in_database(repo_name):
     notion = Client(auth=NOTION_API_KEY)
     return notion.databases.query(DB_ID, filter={"property": "RepoName", "title": {"equals": f"{repo_name}"}}).get("results")
+
+def increase_and_update_row(rows):
+    if rows:
+        row = rows[0]
+        commit_count = row['properties']['CommitCount']['number']
+        update_row(row['id'], "CommitCount", commit_count + 1)
+    else:
+        pprint(f"No rows")
 
 def update_row(page_id, property_name, new_property_value):
     notion = Client(auth=NOTION_API_KEY)
